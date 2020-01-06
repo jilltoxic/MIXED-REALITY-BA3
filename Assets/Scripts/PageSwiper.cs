75281586926a3499a49146ff1f51fcd4876a679a
frzkbl <franziska.blank99@goglemail.com>
2020-01-06 12:22:51 +0100

    adds movement border for swiping, disables swiping, usuage of escape button for pop ups

--- Assets/Scripts/PageSwiper.cs
+++ Assets/Scripts/PageSwiper.cs
@@ -8,10 +8,15 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
     private Vector3 panelLocation;
     public float percentThreshold = 0.2f;
     public float easing = 0.5f;
+    public int leftBorder= 2;
+    public int rightBorder= -1;
+    private int swipeCount;
+    public bool canSwipe;
     // Start is called before the first frame update
     void Start()
     {
         panelLocation = transform.position;
+        canSwipe = true;
     }
 
     // Update is called once per frame
@@ -21,25 +26,38 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
     }
 
     public void OnDrag(PointerEventData data)
-    {
+    { if(!canSwipe)
+        {
+            return;
+        }
+
         float difference = data.pressPosition.x - data.position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
+        if (swipeCount < leftBorder && swipeCount > rightBorder)
+        {
+            
+            transform.position = panelLocation - new Vector3(difference, 0, 0);
+        }
+       
+       
     }
 
 
     public void OnEndDrag(PointerEventData data)
     {
+        if (!canSwipe) return;
         float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
         if (Mathf.Abs(percentage)>= percentThreshold)
         {
             Vector3 newLocation = panelLocation;
-            if (percentage > 0)
+            if (percentage > 0 && swipeCount < leftBorder) //swiping left, panel goes left, i see right
             {
                 newLocation += new Vector3(-Screen.width, 0, 0);
-            }
-            else if (percentage <0)
+                swipeCount++;
+;            }
+            else if (percentage <0 && swipeCount> rightBorder) // swiping right, panel goes right, i see left
             {
                 newLocation += new Vector3(Screen.width, 0, 0);
+                swipeCount--;
             }
             StartCoroutine(SmoothMove(transform.position, newLocation, easing));
             panelLocation = newLocation;
@@ -59,5 +77,9 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
             transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
             yield return null;
         }
+    }
+    public void CanSwipe(bool value)
+    {
+        canSwipe = value;
     }
 }
