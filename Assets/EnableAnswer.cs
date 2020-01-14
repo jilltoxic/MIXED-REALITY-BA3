926ca2c045848d02175c49c909d5ab5148eda8b8
frzkbl <franziska.blank99@goglemail.com>
2020-01-14 14:45:35 +0100

    Adds drop down blue print

---
+++ Assets/EnableAnswer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAnswer : MonoBehaviour
+{
+    public GameObject PanelAnswer;
+    public bool IsOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void YesAnswer()
+    { if (IsOn == false)
+        PanelAnswer.SetActive(true);
+        IsOn = true;
+    }
+    public void NoAnswer()
+    { if (IsOn == true)
+        PanelAnswer.SetActive(false);
+        IsOn = false;
+    }
+}
