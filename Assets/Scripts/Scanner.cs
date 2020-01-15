d319ffcb59c399ae3dfcd340b09c750115129329
valexent <47042660+valexent@users.noreply.github.com>
2020-01-15 13:34:54 +0100

    UI Scanner worketh

---
+++ Assets/Scripts/Scanner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scanner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnLostGame()
+    {
+        //Important: Capital U in UserScore <-- Change Property, not variable
+        if (CurrentUser.instance.UserScore >= 20)
+            CurrentUser.instance.UserScore -= 20;
+        else CurrentUser.instance.UserScore = 0;
+    }
+
+    public void OnWonGame()
+    {
+        //Important: Capital U in UserScore <-- Change Property, not variable
+        CurrentUser.instance.UserScore += 20;
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("UI");
+    }
+}
