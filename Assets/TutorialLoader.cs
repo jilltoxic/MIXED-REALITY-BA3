afb89160422f9dd2cf9e066f054fd15391f74f4c
frzkbl <franziska.blank99@goglemail.com>
2020-01-23 13:30:02 +0100

    Some change

---
+++ Assets/TutorialLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialLoader : MonoBehaviour
+{
+    public float counter;
+    public float LoadNextScene;
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= LoadNextScene)
+        {
+            SceneManager.LoadScene("Tutuorial");
+        }
+    }
+
+}
