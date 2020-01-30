902e26cbaaa5f5ba2e9fd12e3f8a66498b3657dd
nrose <lapislazuli.wittgenstein@googlemail.com>
2020-01-30 14:19:05 +0100

    Sound Intro u Tutorial

---
+++ Assets/Scripts/playTutorialSounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playTutorialSounds : MonoBehaviour
+{
+    void OnAwake()
+    {
+        FindObjectOfType<AudioManager>().Play("UI_Tutorial");
+    }
+}
