2a65cb3663c080338aece02335cb4bc74e541a44
valexent <47042660+valexent@users.noreply.github.com>
2020-02-02 19:10:12 +0100

    shop scene 3d models implemented

--- Assets/Items/ShopPopup.cs
+++ Assets/Items/ShopPopup.cs
@@ -23,20 +23,38 @@ public class ShopPopup : MonoBehaviour
     [SerializeField]
     private Image itemImage;
 
+    [SerializeField]
+    private GameObject newModel;
 
+    [SerializeField]
+    private Camera stageCam;
 
     [SerializeField]
     private GameObject panel;
 
     private Item item;
 
+
+    private void Start()
+    {
+        foreach (Camera cam in Camera.allCameras)
+        {
+            if (cam.gameObject.name == "Stage Camera")
+            {
+                stageCam = cam;
+            }
+        }
+
+    }
     public void SetUp(Item _item)
     {
+        Destroy(newModel);
         item = _item;
         itemName.text = item.itemName;
         itemText.text = item.itemText;
         itemPrice.text = item.itemPrice.ToString();
-        itemImage.sprite = item.itemSprite;
+        //itemImage.sprite = item.itemSprite;
+        newModel = Instantiate(item.modelPrefab, stageCam.transform);
         itemGame.text = item.itemGame;
         panel.SetActive(true);
         
