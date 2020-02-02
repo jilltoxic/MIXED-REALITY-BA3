0645a6defb6fe45dbd7b11201891f464b715de2d
frzkbl <franziska.blank99@goglemail.com>
2020-02-02 15:05:50 +0100

    YOU CAN NOW STOP MUSIC WITH THE SKIP BUTTON IN THE TUTORIAL

---
+++ Assets/DestroyingBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyingBehaviour : MonoBehaviour
+
+{
+
+    private float counter;
+    public float TimeUntilDestroyable;
+    public GameObject IntroSound;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter >= TimeUntilDestroyable)
+        {
+            SceneManager.MoveGameObjectToScene(IntroSound, SceneManager.GetActiveScene());
+        }
+    }
+}
